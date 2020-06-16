0cea172019045904e5224a78fd39629d87017b3a
maypledew <nadjamaykramer@icloud.com>
2020-06-16 02:49:48 +0200

    Heb de gehele map aangepast, triggers gezet voor de ineracties, dialoog toegevoegd voor een NPC en meer pixelart toegevoegd. Ook heb ik de lampen nu goed geanimeerd.

---
+++ ALTD/Assets/Scripts/TimelineTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineTrigger : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    public bool played = false;
+
+    void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            if (!played)
+            {
+                played = true;
+                timeline.Play();
+            }
+        }
+    }
+}
---
+++ ALTD/Assets/Scripts/TimelineTrigger_loop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineTrigger_loop : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            timeline.Play();
+        }
+    }
+}
